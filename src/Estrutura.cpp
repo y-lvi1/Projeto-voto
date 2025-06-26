88a3712d0018ff629c7f39f8704240cb0395ab70
yuri <yuri.levi61@aluno.ifce.edu.br>
2025-06-25 21:42:53 -0300

    Alteração de estrutura

---
+++ src/Estrutura.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+const int ano_atual = 2025;
+
+using namespace std;
+
+string num_eleitor(){
+    srand(time(nullptr));
+    string num = "";
+    num += to_string(rand() % 9 + 1);
+    for (int i = 1; i < 12; ++i)
+        num += to_string(rand() % 10);
+    return num;
+};
+
+struct usuario{
+    string nome;
+    string cpf;
+    string data_nasc;
+    string num_eleitor;
+    int idade;
+};
+
+class Cadastro{
+    private:
+    usuario cadastrado;
+    public:
+    Cadastro(string nome, string cpf, string nasc){
+        cadastrado.nome = nome;
+        cadastrado.cpf = cpf;
+        cadastrado.data_nasc = nasc;
+        cadastrado.num_eleitor = num_eleitor();
+    string ano = nasc.substr(nasc.size() - 4);
+    int ano_nasc = stoi(ano);
+         cadastrado.idade = ano_atual - ano_nasc;
+
+    }
+    void mostrar_dados(){
+        cout << "Nome: " << cadastrado.nome << endl << "CPF: " << cadastrado.cpf << endl << "Idade: " << cadastrado.idade << endl << "Titulo de eleitor : " << cadastrado.num_eleitor;;
+    }
+};
+
+int main() {
+ Cadastro teste("Yuri", "685.492.031-32", "06072004");
+ teste.mostrar_dados();
+}
---
+++ src/Security.cpp
@@ -0,0 +1,38 @@
+#include "Security.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "picosha2.h"
+
+Security::Security()
+{
+    std::ifstream file("config.txt");
+    if (!file.is_open())
+    {
+        std::cout << "Error: O arquivo config.txt não pôde ser aberto!" << std::endl;
+        return;
+    }
+    file >> hash_loaded;
+    return;
+}
+
+bool Security::autenticate_admin()
+{
+    std::string user_input;
+    std::string hash_input;
+
+    std::cout << "Digite a senha de administrador: ";
+    std::cin >> user_input;
+    hash_input = picosha2::hash256_hex_string(user_input);
+
+    if (hash_input == hash_loaded)
+    {
+        std::cout << "Autenticação bem-sucedida!" << std::endl;
+        return true;
+    }
+    else
+    {
+        std::cout << "Senha incorreta!" << std::endl;
+        return false;
+    }
+}
